7fbe3777f92a8fbd1357fb8cdfc147d2ef35ffc9
Mikhail Kolobov <redhead112358@gmail.com>
2023-03-02 01:30:25 +0300

    add hex start land

--- Assets/Prototype/Logic/Items/ItemsSpawner.cs
+++ Assets/Prototype/Logic/Items/ItemsSpawner.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using static Prototype.Logic.Items.ItemsFactory;
 using static UnityEngine.Application;
+using static UnityEngine.Physics;
 using static UnityEngine.Random;
 
 namespace Prototype.Logic.Items
@@ -12,6 +13,7 @@ namespace Prototype.Logic.Items
         [SerializeField] private string[] _itemsNames;
         [SerializeField] private int _startItemsCount;
         [SerializeField] private float _spawnTime;
+        [SerializeField] private Transform _origin;
 
         private readonly List<ItemView> _items = new();
         
@@ -34,12 +36,22 @@ namespace Prototype.Logic.Items
         private void Spawn()
         {
             string itemName = _itemsNames[Range(0, _itemsNames.Length)];
-            Vector3 position = new(Range(-15, 15), 0f, Range(-15, 15));
+            Vector3 position;
+            
+            do
+            {
+                position = _origin.position + new Vector3Int(Range(-15, 15), 0, Range(-15, 15));
+            } while (!InCurrentLand(position));
+            
             ItemView itemView = Create(itemName, position);
 
             _items.Add(itemView);
             
             //Log($"[ItemsSpawner] spawn {itemName} at {position}");
         }
+
+        private bool InCurrentLand(Vector3 position) =>
+            Raycast(position, Vector3.down, out RaycastHit hit)
+            && hit.transform.IsChildOf(transform);
     }
 }
